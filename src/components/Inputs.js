0cb2edd8d908cfd3648f215de37364d941793853
Chillup <philipkaim@gmail.com>
2018-01-31 19:58:30 -0500

    commented out custom input form and its logic

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -30,27 +30,32 @@ class Inputs extends React.Component {
         }));
     }
 
-    handleCustomSession = (e) => {
-        this.setState(() => ({
-            session: e.target.value
-        }));
-    }
+    // handleCustomSession = (e) => {
+    //     this.setState(() => ({
+    //         session: e.target.value
+    //     }));
+    // }
 
-    handleCustomBreak = (e) => {
-        this.setState(() => ({
-            break: e.target.value
-        }));
-    }
+    // handleCustomBreak = (e) => {
+    //     e.preventDefault();
+
+    //     const customBreak = e.target.elements.customBreak.value.trim();
+    //     const setBreak = this.handleCustomBreak(customBreak);
+
+    //     this.setState(() => ({
+    //         break: setBreak 
+    //     }));
+    // }
 
     render() {
         return (
             <div className='inputs'>
                 <div className='inputs__session'>
                     <p>Session Length</p>
-                    <div className="custom-session">
+                    {/* <div className="custom-session">
                         <label htmlFor="custom-session">Custom Session:</label>
-                        <input type="text" id='custom-session' onBlur={this.handleCustomSession} />
-                    </div>
+                        <input type="text" id='custom-session' onChange={this.handleCustomSession.bind(this)} />
+                    </div> */}
                     <button id='increment-session' onClick={this.handleIncrementSession}>+</button>
                     <p>{this.state.session}</p>
                     <button id='decrement-session' onClick={this.handleDecramentSession}>-</button>
@@ -58,10 +63,11 @@ class Inputs extends React.Component {
 
                 <div className='inputs__break'>
                     <p>Break Length</p>
-                    <div className='custom-break'>
+                    {/* <form className='custom-break' onSubmit={this.handleCustomBreak}>
                         <label htmlFor="custom-break">Custom Break:</label>
-                        <input type="text" id='custom-break' onBlur={this.handleCustomBreak} />
-                    </div>
+                        <input type="text" id='custom-break' name='customBreak' />
+                        <button>Add Custom Session</button>
+                    </form> */}
                     <button id='increment-break' onClick={this.handleIncrementBreak}>+</button>
                     <p>{this.state.break}</p>
                     <button id='decrement-break' onClick={this.handleDecramentBreak}>-</button>
