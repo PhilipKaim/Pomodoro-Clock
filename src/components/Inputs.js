f686efbff00f64c5eaba7ea0b0aec39a355f609e
Chillup <philipkaim@gmail.com>
2018-01-31 19:31:55 -0500

    input increment and decrament buttons working

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,6 +1,46 @@
 import React from 'react';
 
 class Inputs extends React.Component {
+    state = {
+        session: 30,
+        break: 10
+    };
+
+    handleIncrementSession = () => {
+        this.setState((prevState) => ({
+            session: prevState.session + 1
+        }));
+    }
+
+    handleDecramentSession = () => {
+        this.setState((prevState) => ({
+            session: prevState.session - 1
+        }));
+    }
+
+    handleIncrementBreak = () => {
+        this.setState((prevState) => ({
+            break: prevState.break + 1
+        }));
+    }
+
+    handleDecramentBreak = () => {
+        this.setState((prevState) => ({
+            break: prevState.break - 1
+        }));
+    }
+
+    handleCustomSession = (e) => {
+        this.setState(() => ({
+            session: e.target.value
+        }));
+    }
+
+    handleCustomBreak = (e) => {
+        this.setState(() => ({
+            break: e.target.value
+        }));
+    }
 
     render() {
         return (
@@ -9,22 +49,22 @@ class Inputs extends React.Component {
                     <p>Session Length</p>
                     <div className="custom-session">
                         <label htmlFor="custom-session">Custom Session:</label>
-                        <input type="text" id='custom-session' />
+                        <input type="text" id='custom-session' onBlur={this.handleCustomSession} />
                     </div>
-                    <button id='increment-session'>+</button>
-                    <p>5</p>
-                    <button id='decrement-session'>-</button>
+                    <button id='increment-session' onClick={this.handleIncrementSession}>+</button>
+                    <p>{this.state.session}</p>
+                    <button id='decrement-session' onClick={this.handleDecramentSession}>-</button>
                 </div>
 
                 <div className='inputs__break'>
                     <p>Break Length</p>
                     <div className='custom-break'>
                         <label htmlFor="custom-break">Custom Break:</label>
-                        <input type="text" id='custom-break' />
+                        <input type="text" id='custom-break' onBlur={this.handleCustomBreak} />
                     </div>
-                    <button id='increment-break'>+</button>
-                    <p>25</p>
-                    <button id='decrement-break'>-</button>
+                    <button id='increment-break' onClick={this.handleIncrementBreak}>+</button>
+                    <p>{this.state.break}</p>
+                    <button id='decrement-break' onClick={this.handleDecramentBreak}>-</button>
                 </div>
             </div>
         );
